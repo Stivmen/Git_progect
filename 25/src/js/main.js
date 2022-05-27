424fa2d5b690645448c7f711a6b506baee895dc5
Stanislav Panasiuk <Stivmen97@gmail.com>
2022-05-27 18:14:41 +0300

    add 2/3 HW

---
+++ 25/src/js/main.js
@@ -0,0 +1,45 @@
+//Мінімум:
+//
+//
+let name = 'Grisha';
+let nameFirst = 'Pupkin';
+let nameLast = 'StGrigorovich';
+let age = 100;
+
+// let let = 1111;
+// let const = 1212;
+// let var = vasya;
+// let a = a;
+// let a = b;
+// let alert = alert;
+
+//Перший вид комментарів
+/*
+Другий вид комментарів)))
+*/
+
+
+// види написання імен 1) nameOne 2) name_one
+//Тобто "камел сейс" та "нижнє підкреслювання"("кебаб стайл")
+
+
+//Норма
+
+let firstName = prompt ('Привіт! Як тебе звати?');
+alert('Привіт'+ ' ' + firstName);;
+
+let secondName = prompt('Яке твоє Прізвище?');
+alert('Гарне Прізвище -' + ' ' + secondName);
+
+const year = 2022;
+
+let dateOfBirth = prompt('В якому році ти народився?');
+alert('Тобто тобі - ' + (year - dateOfBirth) + ' ' + 'років!');
+
+let formulSquare = prompt('Назви довжину сторони квадрата в міліметрах.');
+alert('Периметр цього квадрата дорівнює - ' + (formulSquare * 4) + 'mm');
+
+// Максимум
+
+let radius = prompt('Назви радіус кола')
+alert('Радіус кола дорівнює ')
