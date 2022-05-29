b397e531d5634104bdde95c4f6feadcb78f5ab58
Stanislav Panasiuk <Stivmen97@gmail.com>
2022-05-29 10:14:48 +0300

    fix

--- 26/main.js
+++ 26/main.js
@@ -5,25 +5,25 @@
 // let b = 2;
 // alert(+a + b);
 
-// let gb = prompt('введіть обьем флеш карти в гігабайтах');
-// let obiem = (gb * 1024) / 820;
-// alert ('На вашу флеш карту поміститься ' + Math.trunc(obiem) + ' ' + 'файлів розміром 820мб');
+let gb = prompt('введіть обьем флеш карти в гігабайтах');
+let obiem = (gb * 1024) / 820;
+alert ('На вашу флеш карту поміститься ' + Math.trunc(obiem) + ' ' + 'файлів розміром 820мб');
 
 
-// let summaGrn = prompt('Введіть сумму грошей');
-// let price = prompt('введіть ціну шоколадки');
-// let namber = (summaGrn / price);
-// let balance = (summaGrn % price);
-// alert(Math.trunc(namber) + ' ' + "Шоколадок ви зможете придбати." + ' ' + balance + 'грн ' + 'Ваш залишок.');
-//345
+let summaGrn = prompt('Введіть сумму грошей');
+let price = prompt('введіть ціну шоколадки');
+let namber = (summaGrn / price);
+let balance = (summaGrn % price);
+alert(Math.trunc(namber) + ' ' + "Шоколадок ви зможете придбати." + ' ' + balance + 'грн ' + 'Ваш залишок.');
+345
 
-// let treeNamber = prompt('Введіть тризначне число');
-// let aaa = treeNamber % 10; //5
-// let bbb = (treeNamber / 10); 
-// let bbb2 =(Math.trunc(bbb) % 10); //4
-// let ccc = (treeNamber / 100);
-// let ccc2 = (Math.trunc(ccc) % 10);
-// alert(aaa + '' + bbb2 + '' + ccc2);
+let treeNamber = prompt('Введіть тризначне число');
+let aaa = treeNamber % 10; //5
+let bbb = (treeNamber / 10); 
+let bbb2 =(Math.trunc(bbb) % 10); //4
+let ccc = (treeNamber / 100);
+let ccc2 = (Math.trunc(ccc) % 10);
+alert(aaa + '' + bbb2 + '' + ccc2);
 
 let contribution = prompt('Введіть сумму вкладу')
 let interestamount = (contribution * 0.05) / 6;
